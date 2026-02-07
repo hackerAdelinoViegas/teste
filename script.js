7637cdc3d727fb3300c78c91021490db0f3a364e
hackerAdelinoViegas <adelinoviegasdev@gmail.com>
2026-02-07 21:07:37 +0100

    q3

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function show() {
   console.log("Sou Engenheiro de Software");
   console.log("nova linha");
   console.log("Extremamnente duro!!!");
+  console.log("pouco a pouco tu vais chegar lá");
 }
